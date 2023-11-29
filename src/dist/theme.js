af54643228c2923d09c03fa26f4da7eaba28167b
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2023-11-29 08:40:45 +0200

    add relevant dependencies

---
+++ src/dist/theme.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var material_1 = require("@mui/material");
+exports.theme = material_1.createTheme({
+    palette: {
+        primary: {
+            main: "#1760a5",
+            light: "skyblue"
+        },
+        secondary: {
+            main: '#15c630'
+        }
+    }
+});
+
+//# sourceMappingURL=theme.js.map
